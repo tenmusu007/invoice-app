61cb323cf775751e06aa7d7c66235dbf8a258ead
Atsuya Tanaka <tenmusu007@gmail.com>
2023-01-06 17:15:56 -0800

    create link compo

--- components/InvoiceCard/index.tsx
+++ components/InvoiceCard/index.tsx
@@ -1,18 +1,21 @@
+import Link from "@components/Link";
 import Text from "@components/Text";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-const InvoiceCard = (props:any) => {
+const InvoiceCard = (props: any) => {
 	const TextStyle = { textAlign: "center", marginY: 2 };
 	const { no, compnay_name, date } = props.list;
 	return (
-		<Card sx={{ width: 150, height: 180 }}>
-			<CardContent>
-				<Text text={`No${no}`} variant={"body1"} style={TextStyle} />
-				<Text text={compnay_name} variant={"body1"} style={TextStyle} />
-				<Text text={date} variant={"body1"} style={TextStyle} />
-			</CardContent>
-		</Card>
+		<Link path={compnay_name}>
+			<Card sx={{ width: 150, height: 180 }}>
+				<CardContent>
+					<Text text={`No${no}`} variant={"body1"} style={TextStyle} />
+					<Text text={compnay_name} variant={"body1"} style={TextStyle} />
+					<Text text={date} variant={"body1"} style={TextStyle} />
+				</CardContent>
+			</Card>
+		</Link>
 	);
 };
 
