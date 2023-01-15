adddc978f0d23318375e7565abd38d84f3d0fb00
Atsuya Tanaka <tenmusu007@gmail.com>
2023-01-15 09:13:56 -0800

    create duumy and use for mylist

--- components/Molecules/InvoiceCard/index.tsx
+++ components/Molecules/InvoiceCard/index.tsx
@@ -5,14 +5,22 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 const InvoiceCard = (props: any) => {
 	const TextStyle = { textAlign: "center", marginY: 2 };
-	const { no, company_name, date } = props.list;
+	const { businessInfo, invocieNumber, issued,} = props.list;
 	return (
-		<Link path={company_name}>
+		<Link path={invocieNumber}>
 			<Card sx={{ width: 150, height: 180 }}>
 				<CardContent>
-					<Text text={`No${no}`} variant={"body1"} style={TextStyle} />
-					<Text text={company_name} variant={"body1"} style={TextStyle} />
-					<Text text={date} variant={"body1"} style={TextStyle} />
+					<Text
+						text={`No${invocieNumber}`}
+						variant={"body1"}
+						style={TextStyle}
+					/>
+					<Text
+						text={businessInfo.companyName}
+						variant={"body1"}
+						style={TextStyle}
+					/>
+					<Text text={issued} variant={"body1"} style={TextStyle} />
 				</CardContent>
 			</Card>
 		</Link>
--- pages/mylist.tsx
+++ pages/mylist.tsx
@@ -1,16 +1,16 @@
 import InvoiceCard from "@components/Molecules/InvoiceCard";
 import { Grid } from "@mui/material";
-import dummyData from "@components/Molecules/InvoiceCard/dummy.json";
+import dummyData from "../data/mylist.json";
 const mylist = () => {
-	const { list } = dummyData.data;
+	const { list }: any = dummyData.data;
 	return (
 		<>
 			<Grid container spacing={4}>
 				{list.map((item: any) => {
 					return (
-							<Grid item xs={3} key={item.id}>
-								<InvoiceCard list={item} key={item.id} />
-							</Grid>
+						<Grid item xs={3} key={item.id}>
+							<InvoiceCard list={item} key={item.id} />
+						</Grid>
 					);
 				})}
 			</Grid>
@@ -19,3 +19,4 @@ const mylist = () => {
 };
 
 export default mylist;
+ 
