548fd925ca2048cc0fb83f9b862a26866b55c540
Yuto Yamakita <yuvto.0420@gmail.com>
2023-01-21 22:45:38 -0800

    Implemented adding new form function

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -3,7 +3,7 @@ import { Button as MUIButton } from "@mui/material";
 import { Button as ButtonType } from "types/button";
 
 const Button = ({ text, onClick, sx, variant, type }: ButtonType) => {
-  return <MUIButton type={type}>{text}</MUIButton>;
+  return <MUIButton type={type} onClick={onClick}>{text}</MUIButton>;
 };
 
 export default Button;
--- components/Molecules/descriptionForm/index.tsx
+++ components/Molecules/descriptionForm/index.tsx
@@ -4,6 +4,7 @@ import { useFormContext, useFieldArray, useForm } from "react-hook-form";
 import { Description as DescriptionType } from "types/description";
 import { Grid } from "@mui/material";
 import TotalContext, { useTotalContext } from "Context/TotalContext";
+import Button from "@components/Button";
 
 const DescriptionForm = () => {
   const defaultValues = {
@@ -14,12 +15,14 @@ const DescriptionForm = () => {
   const { register, control } = useFormContext<DescriptionType>();
   // const { register } = useForm<DescriptionType>({defaultValues});
   const totalContext = useContext(useTotalContext);
-  //MUI使わんからこれは要らん
-  // const { fields, append, remove } = useFieldArray({
-  //   name: "description.amount",
-  //   control
-  // })
-  // console.log(totalContext)
+  //もしAppendが無理ならHardcodingする。（8行くらい）でもRemove機能は必須
+  //追加できた
+  //Append: Add new element to field
+  const { fields, append, remove } = useFieldArray({
+    name: "items",
+    control,
+  });
+  console.log("fields", fields);
   const sumUpTotal = (event: any) => {
     setInputValues([Number(event.target.value)]);
     console.log(event.target.value);
@@ -114,6 +117,70 @@ const DescriptionForm = () => {
           />
         </Grid>
       </Grid>
+      {fields.map((field, index) => {
+        return (
+          <React.Fragment key={field.id}>
+            <Grid container spacing={1}>
+              <Grid item xs={4}>
+                <Input
+                  name="description.item"
+                  type="text"
+                  placeholder="Enter Item name/Description"
+                  register={register}
+                />
+              </Grid>
+              <Grid item xs={2}>
+                <Input
+                  name="description.quantity"
+                  type="number"
+                  placeholder="1"
+                  register={register}
+                />
+              </Grid>
+              <Grid item xs={2}>
+                <Input
+                  name="description.unitPrice"
+                  type="number"
+                  placeholder="$0"
+                  register={register}
+                />
+              </Grid>
+              <Grid item xs={2}>
+                <Input
+                  name="description.tax"
+                  type="number"
+                  placeholder="15%"
+                  register={register}
+                />
+              </Grid>
+              <Grid item xs={2}>
+                <Input
+                  name="description.amount"
+                  type="number"
+                  placeholder="$100"
+                  register={register}
+                  onChange={(e: any) => sumUpTotal(e)}
+                />
+              </Grid>
+            </Grid>
+            {/* <input {...register(`items.${index}.name`)} /> */}
+          </React.Fragment>
+        );
+      })}
+      <Button
+        text={"Add row"}
+        onClick={() => {
+          append({
+            name: "",
+            quantity: 0,
+            unitPrice: 0,
+            tax: 0,
+            amount: 0,
+          }),
+          console.log(fields)
+        }
+        }
+      />
     </>
   );
 };
