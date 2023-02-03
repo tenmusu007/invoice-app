feb2d5289ff9fae60eb2bd4ed8a004ca2545a4e9
Atsuya Tanaka <tenmusu007@gmail.com>
2023-02-03 14:56:31 -0800

    add google btn

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,16 +1,44 @@
 import React from "react";
-import { Button, Typography } from "@mui/material";
-import { signIn} from "next-auth/react";
-
+import { Button, Grid, Typography } from "@mui/material";
+import { signIn } from "next-auth/react";
+import Text from "@components/atoms/Text";
+import Image from "next/image";
+import Illustrationtop from "public/Illustrationtop.jpg";
+import GoogleButton from "react-google-button";
 const Login = () => {
 	const handleGoggle = () => {
 		signIn("google", { callbackUrl: process.env.NEXT_PUBLIC_BASE_URL });
-	}
+	};
 	return (
 		<>
-			<Typography variant='h2'>Login</Typography>
-			<Button onClick={handleGoggle}>Next Google</Button>
-
+			<Grid container>
+				<Grid container item lg={6} alignItems='center' display='flex'>
+					<Grid item md={12}>
+						<Text text={"Welcom to Invoicer"} variant={"h3"} />
+					</Grid>
+					<Grid item md={12}>
+						<Text
+							text={"Your free invoice generator is around the corner!!"}
+							variant={"h5"}
+						/>
+					</Grid>
+					<Grid item md={12}>
+						<Image src={Illustrationtop} alt={"image"} width={300} />
+					</Grid>
+				</Grid>
+				<Grid container item lg={6} alignItems='center' display='flex'>
+					<Grid item md={12}>
+						<Text
+							text={"Please Sign-in with your Google here."}
+							variant={"h5"}
+							style={{ textAlign: "center" }}
+						/>
+					</Grid>
+					<Grid item md={12} sx={{ textAlign: "center" }}>
+						<GoogleButton onClick={handleGoggle} />
+					</Grid>
+				</Grid>
+			</Grid>
 		</>
 	);
 };
