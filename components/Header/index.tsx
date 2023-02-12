14ce6343084a90c5b8515c9239b1469ade8427f2
Atsuya Tanaka <tenmusu007@gmail.com>
2023-02-12 15:34:28 -0800

    fix folder issues

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -6,69 +6,77 @@ import Text from "@components/atoms/Text";
 import { title } from "process";
 import LinkTag from "@components/atoms/Link";
 const Header = () => {
-  const authContext = useContext(useAuthContext);
-  const { data: session } = useSession();
+	const authContext = useContext(useAuthContext);
+	const { data: session } = useSession();
 
-  const nextAuthLogout = () => {
-    signOut({ callbackUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/login` });
-  };
-  return (
-    <>
-      <Grid container sx={{ width: "100%", marginX: "auto", marginY: 2 }}>
-        <Grid container item sm={3} sx={{ justifyContent: "space-around" }}>
-          <Text variant={"h5"} text={"title"} />
-        </Grid>
-        <Grid container item sm={5} sx={{ justifyContent: "space-around" }}>
-          <Grid item sm={3} style={{ textAlign: "center" }}>
-            <Button> Create</Button>
-          </Grid>
-          <Grid item sm={3}>
-            <LinkTag path={"/mylist"}>
-              <Text
-                variant={"h6"}
-                text={"MyList"}
-                style={{ textAlign: "center" }}
-              />
-            </LinkTag>
-          </Grid>
-          <Grid item sm={3}>
-            <LinkTag path={"/setting"}>
-              <Text
-                variant={"h6"}
-                text={"Setting"}
-                style={{ textAlign: "center" }}
-              />
-            </LinkTag>
-          </Grid>
-        </Grid>
-        <Grid container item sm={4} alignItems={"center"}>
-          {session?.user?.name ? (
-            <>
-              <Grid item sm={6}>
-                <Text
-                  label={"body1"}
-                  text={session?.user?.name}
-                  style={{ textAlign: "center" }}
-                />
-              </Grid>
-              <Grid item sm={6} style={{ textAlign: "center" }}>
-                <Button onClick={nextAuthLogout}>Logout</Button>
-              </Grid>
-            </>
-          ) : (
-            <>
-              <Grid item sm={4}>
-                <Button> login</Button>
-              </Grid>
-              <Grid item sm={4}>
-                <Button> signup</Button>
-              </Grid>
-            </>
-          )}
-        </Grid>
-      </Grid>
-    </>
-  );
+	const nextAuthLogout = () => {
+		signOut({ callbackUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/login` });
+	};
+	return (
+		<>
+			<Grid
+				container
+				sx={{ width: "100%", marginX: "auto", marginY: 2 }}
+				position={"fixed"}
+			>
+				<Grid item sm={3} sx={{ justifyContent: "space-around" }}>
+					<Text variant={"h5"} text={"title"} />
+				</Grid>
+				<Grid container item sm={5} sx={{ justifyContent: "space-around" }}>
+					<Grid item sm={3} style={{ textAlign: "center" }}>
+						<Button> Create</Button>
+					</Grid>
+					<Grid item sm={3}>
+						<LinkTag path={"/mylist"}>
+							<Text
+								variant={"h6"}
+								text={"MyList"}
+								style={{ textAlign: "center" }}
+							/>
+						</LinkTag>
+					</Grid>
+					<Grid item sm={3}>
+						<LinkTag path={"/profile"}>
+							<Text
+								variant={"h6"}
+								text={"Profile"}
+								style={{ textAlign: "center" }}
+							/>
+						</LinkTag>
+					</Grid>
+				</Grid>
+				<Grid container item sm={4} alignItems={"center"}>
+					{session?.user?.name ? (
+						<>
+							<Grid item sm={6}>
+								<Text
+									label={"body1"}
+									text={session?.user?.name}
+									style={{ textAlign: "center" }}
+								/>
+							</Grid>
+							<Grid item sm={6} style={{ textAlign: "center" }}>
+								<Button onClick={nextAuthLogout}>Logout</Button>
+							</Grid>
+						</>
+					) : (
+						<>
+							<Grid item sm={4}>
+								<LinkTag path={"/login"}>
+									<Button> login</Button>
+								</LinkTag>
+							</Grid>
+							<Grid item sm={4}>
+								<LinkTag path={"/signup"}>
+									<Button> signup</Button>
+								</LinkTag>
+							</Grid>
+						</>
+					)}
+				</Grid>
+			</Grid>
+		</>
+	);
 };
 
 export default Header;
