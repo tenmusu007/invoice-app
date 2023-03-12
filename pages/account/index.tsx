29f8f957e9225071576b9b5b831c72caea367b4c
Yuto Yamakita <yuvto.0420@gmail.com>
2023-03-12 14:34:16 -0700

    Fixed minor issues

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -7,26 +7,28 @@ import { useLocale } from 'helper/useLocale';
 import ProfileText from './text.json';
 import { Box, Grid } from '@mui/material';
 import Button from '@src/components/atoms/Button';
-import Modal from '@src/components/organisms/Modal';
 import BillToTemplate from '@src/pages/Account/Template/billToTemplate';
-import BillToForm from '@src/components/Molecules/billToForm';
-import { FormProvider, useForm } from 'react-hook-form';
 const Setting = (props: any) => {
   const { data } = props;
   const router = useRouter();
-  const textAline = { textAlign: 'center' };
+  const textAline = { textAlign: 'center' } as const;
+  const textStyle = {
+    width: '45%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginX: 'auto',
+    marginY: '30px',
+  } as const;
   const [language, setLanguage] = useState('');
   const { t } = useLocale(ProfileText);
   useEffect(() => {
     setSetting(t);
   }, [data, language]);
   const [setting, setSetting] = useState<any>(t);
-  const [openModal, setOpenModal] = useState<boolean>(false);
-  const handleOpen = () => setOpenModal(true);
-  const handleClose = () => setOpenModal(false);
-
-  const methods = useForm();
-  const { handleSubmit, reset } = methods;
+  const [openBillToModal, setOpenBillToModal] = useState<boolean>(false);
+  const handleBillToOpen = () => setOpenBillToModal(true);
+  const handleBillToClose = () => setOpenBillToModal(false);
 
   return (
     <>
@@ -37,29 +39,11 @@ const Setting = (props: any) => {
             labelText={t.setting.name}
             variant={'body1'}
             text={'Astuya'}
-            style={{
-              width: '45%',
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              marginX: 'auto',
-              marginY: '30px',
-            }}
+            style={textStyle}
           />
         </Grid>
         <Grid item md={12}>
-          <Text
-            label={'h5'}
-            labelText={t.setting.language}
-            style={{
-              width: '45%',
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              marginX: 'auto',
-              marginY: '30px',
-            }}
-          >
+          <Text label={'h5'} labelText={t.setting.language} style={textStyle}>
             <SelectInput
               items={t.setting.langchoice}
               name={'language'}
@@ -69,28 +53,17 @@ const Setting = (props: any) => {
           </Text>
         </Grid>
         <Grid item md={12}>
-          <Text variant={'h6'} text={t.setting.bill} style={textAline} />
           <Text variant={'h6'} text={t.setting.info} style={textAline} />
-          {/* <FormProvider {...methods}>
-            <Button text={'Bill to'} onClick={handleOpen} />
-            <Modal openModal={openModal} setOpenModal={handleClose}>
-              <BillToForm />
-              <Box width={2}>
-                <Button
-                  text={'Submit'}
-                  type="submit"
-                  sx={{
-                    background: '#EEBBC3',
-                    color: '#232946',
-                    borderRadius: 2,
-                    fontWeight: 'bold',
-                  }}
-                />
-              </Box>
-            </Modal>
-          </FormProvider> */}
-          <Button text={'Bill to'} onClick={handleOpen} />
-          <BillToTemplate openModal={openModal} setOpenModal={handleClose} ></BillToTemplate>
+          <Button
+            variant={'outlined'}
+            sx={textAline}
+            text={t.setting.bill}
+            onClick={handleBillToOpen}
+          />
+          <BillToTemplate
+            openModal={openBillToModal}
+            setOpenModal={handleBillToClose}
+          ></BillToTemplate>
         </Grid>
       </Grid>
     </>
