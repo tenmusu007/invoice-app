f741a978d7318bffc2b2a2eb19b91876d2f6d966
Atsuya Tanaka <tenmusu007@gmail.com>
2023-03-25 22:01:09 -0700

    fix getUserData

--- pages/api/account/user.ts
+++ pages/api/account/user.ts
@@ -1,4 +1,7 @@
 import UserInFo from '@models/account';
+import BankInfo from '@models/bankInfo';
+import Bills from '@models/bills';
+import BusinessInfo from '@models/businessInfo';
 import Users from '@models/user';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { getToken } from 'next-auth/jwt';
@@ -10,14 +13,29 @@ export default async function getUserData(req: NextApiRequest, res: NextApiRespo
     const userInfo = await UserInFo.findOne({
       userId: cuurentUser[0]._id.toString(),
     });
+    const businessInfo = []
+    const billTo = []
+    const bankInfo = []
+    for (const i of userInfo.businessInfo) {
+      const temp = await BusinessInfo.findById(userInfo.businessInfo[i]);
+      businessInfo.push(temp);
+    }
+    for (const i of userInfo.billTo) {
+      const temp = await Bills.findById(userInfo.billTo[i]);
+      billTo.push(temp);
+    }
+    for (const i of userInfo.bankInfo) {
+      const temp = await BankInfo.findById(userInfo.bankInfo[i]);
+      bankInfo.push(temp);
+    }
     //TODO テンプレートの各idを取ってくる
     res.status(200).json({
       name: cuurentUser[0].name,
       image: cuurentUser[0].image,
       language: userInfo.language,
-      businessInfo: [],
-      billTo: [],
-      bankInfo:[]
+      businessInfo: businessInfo,
+      billTo: billTo,
+      bankInfo: bankInfo,
     });
   } catch (error) {
     res.status(400).json(error);
