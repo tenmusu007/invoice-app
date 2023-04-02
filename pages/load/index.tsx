b6c8ee44f3d6eabc0f51120120cb2500bb7545f5
Yuto Yamakita <yuvto.0420@gmail.com>
2023-04-02 11:42:51 -0700

    Add a style for PDF

---
+++ pages/load/index.tsx
@@ -0,0 +1,26 @@
+import Text from 'src/components/atoms/Text';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+const Load = () => {
+  const { data: session, status } = useSession();
+  const [user, setUser] = useState<any>();
+  const router = useRouter();
+
+  useEffect(() => {
+    const loginPath = () => {
+      if (status === 'authenticated') {
+        // fetch();
+        return router.push('/home');
+      } else {
+        console.log(status);
+        return router.push('/login');
+      }
+    };
+    loginPath();
+  }, []);
+  return <Text variant="h2" text="sushi" />;
+};
+
+export default Load;
--- src/components/organisms/PDFView/index.tsx
+++ src/components/organisms/PDFView/index.tsx
@@ -12,37 +12,63 @@ import dummyInvoice from 'mocks/dummyInvoice.json';
 import { Invoice as InvoiceType } from 'types/inputValue';
 
 const styles = StyleSheet.create({
-  section: { textAlign: 'center', margin: 30 },
-  heading: { fontSize: '32px' },
+  page: { paddingTop: 200 },
+  section: { margin: '24px', marginTop: 36 },
+  heading: { fontSize: '24px' },
+  title: {
+    fontSize: '32px',
+    textAlign: 'left',
+    letterSpacing: '2px',
+    fontWeight: 'bold',
+  },
+  info: {
+    textAlign: 'right',
+  },
+  flex: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
 });
 
 const PDF = () => {
-  const [ invoice, setInvoice ] = useState<InvoiceType>()
+  const [invoice, setInvoice] = useState<InvoiceType>();
   return (
     <Document>
       <Page size="A4">
         <View style={styles.section}>
           {dummyInvoice.invoice.data.map((data) => (
             <div key={data.invocieNumber}>
-              <Text>{data.invocieNumber}</Text>
-              <Text>{data.issued}</Text>
-              <Text>{data.due}</Text>
-              <View style={styles.heading}>
-                <Text>Business Info</Text>
+              <View>
+                <Text style={styles.title}>Invoice</Text>
               </View>
-              <Text>{data.businessInfo.companyName}</Text>
-              <Text>{data.businessInfo.address}</Text>
-              <Text>{data.businessInfo.city}</Text>
-              <Text>{data.businessInfo.province}</Text>
-              <Text>{data.businessInfo.country}</Text>
-              <View style={styles.heading}>
-                <Text>Bill To</Text>
+              <View style={styles.info}>
+                <Text>{data.invocieNumber}</Text>
+                <Text>{data.issued}</Text>
+                <Text>{data.due}</Text>
               </View>
-              <Text>{data.billTo.companyName}</Text>
-              <Text>{data.billTo.addressLine1}</Text>
-              <Text>{data.billTo.city}</Text>
-              <Text>{data.billTo.province}</Text>
-              <Text>{data.billTo.country}</Text>
+              <View style={styles.flex}>
+                <View style={{ width: '45%'}}>
+                  <View style={styles.heading}>
+                    <Text>Bill To:</Text>
+                  </View>
+                  <Text>{data.billTo.companyName}</Text>
+                  <Text>{data.billTo.addressLine1}</Text>
+                  <Text>{data.billTo.city}</Text>
+                  <Text>{data.billTo.province}</Text>
+                  <Text>{data.billTo.country}</Text>
+                </View>
+                <View style={{ width: '45%'}}>
+                  <View style={styles.heading}>
+                    <Text>Business Info:</Text>
+                  </View>
+                  <Text>{data.businessInfo.companyName}</Text>
+                  <Text>{data.businessInfo.address}</Text>
+                  <Text>{data.businessInfo.city}</Text>
+                  <Text>{data.businessInfo.province}</Text>
+                  <Text>{data.businessInfo.country}</Text>
+                </View>
+              </View>
             </div>
           ))}
         </View>
@@ -59,7 +85,9 @@ const PDFView = () => {
   }, []);
   return (
     <div>
-      <PDFViewer style={{ height: '1400px', width: '1400px' }}>
+      <PDFViewer
+        style={{ height: window.innerWidth, width: window.innerWidth }}
+      >
         <PDF />
       </PDFViewer>
     </div>
