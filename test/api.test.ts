6533ac69b81bf9b02497c8ae078c1d89422da0de
Atsuya Tanaka <tenmusu007@gmail.com>
2023-04-27 18:07:41 -0700

    add

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,13 +1,16 @@
 // import { ApiInstance } from '../helper/ApiInstance';
-// export {};
-// describe('API', () => {
-//   test('should return a 200', async () => {
-//     const mockData = { invoiceId: '6443617ceb584c09d26e6a19' };
-//     const response = await ApiInstance({
-//       method: 'post',
-//       url: 'mylist/get_invoice_data',
-//       data: mockData,
-//     });
-//     expect(response.status).toBe(200);
-//   });
-// });
+export {};
+describe('API', () => {
+  test('should return a 200', async () => {
+    // const mockData = { invoiceId: '6443617ceb584c09d26e6a19' };
+    // const response = await ApiInstance({
+    //   method: 'post',
+    //   url: 'mylist/get_invoice_data',
+    //   data: mockData,
+    // });
+    // expect(response.status).toBe(200);
+    // eslint-disable-next-line no-console
+    console.log('test');
+    
+  });
+});
