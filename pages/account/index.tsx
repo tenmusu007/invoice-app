6fb70d466ee6230d1495ccc6d324f8d2f71420d5
Atsuya Tanaka <tenmusu007@gmail.com>
2023-05-19 16:08:39 -0700

    fix for deploy

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -3,19 +3,18 @@ import { GetServerSideProps } from 'next/types';
 import AccountPage from '@src/pages/account';
 
 type Props = {
-  data : string
-}
+  data: string;
+};
 const Account = (props: Props) => (
-    <>
-      <AccountPage locale={props.data} />
-    </>
-  );
+  <>
+    <AccountPage locale={props.data} />
+  </>
+);
 
 export default Account;
 
-
 export const getServerSideProps: GetServerSideProps = async (context) => ({
-    props: {
-      data: context.locale,
-    },
-  });
+  props: {
+    data: context.locale,
+  },
+});
