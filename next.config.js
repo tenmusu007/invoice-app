5efc625836d46d34cfd746034cf08b828234751b
Atsuya Tanaka <tenmusu007@gmail.com>
2023-05-20 01:33:04 -0700

    fix fav

--- next.config.js
+++ next.config.js
@@ -41,6 +41,19 @@ const nextConfig = {
     });
     return config;
   },
+  async headers() {
+    return [
+      {
+        source: '/favicon.ico',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
