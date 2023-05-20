6021b3d3c3cd3e044b4067cdf7584f4533114ab9
Atsuya Tanaka <tenmusu007@gmail.com>
2023-05-19 17:14:37 -0700

    fix signup

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,55 +1,9 @@
 import { Box, Grid } from '@mui/material';
 
-import Image from 'next/image';
-import { signIn } from 'next-auth/react';
 import React from 'react';
-import GoogleButton from 'react-google-button';
 
-import SignupText from './text.json';
+import SignUpPage from '@src/pages/signup';
 
-import { useLocale } from 'helper/useLocale';
-import Illustrationtop from 'public/Illustrationtop.jpg';
-import Text from 'src/components/atoms/Text';
-
-const SignUp = () => {
-  // const { data: session } = useSession();
-  const handleGoggle = () => {
-    signIn('google', { callbackUrl: process.env.NEXT_PUBLIC_BASE_URL });
-  };
-  const { t } = useLocale(SignupText);
-
-  return (
-    <Grid container>
-      <Grid
-        container
-        item
-        lg={6}
-        alignItems="center"
-        display="flex"
-        spacing={4}
-      >
-        <Grid item md={12}>
-          <Text text={t.headline} variant={'h3'} />
-        </Grid>
-        <Grid item md={12}>
-          <Text text={t.desc} variant={'h5'} />
-        </Grid>
-        <Grid item md={12}>
-          <Image src={Illustrationtop} alt={'image'} width={300} />
-        </Grid>
-      </Grid>
-      <Grid container item lg={6} alignItems="center" display="flex">
-        <Grid item md={12}>
-          <Text text={t.btn} variant={'h5'} style={{ textAlign: 'center' }} />
-        </Grid>
-        <Grid item md={12} sx={{ textAlign: 'center' }}>
-          <Box display={'flex'} justifyContent={'center'}>
-            <GoogleButton onClick={handleGoggle} />
-          </Box>
-        </Grid>
-      </Grid>
-    </Grid>
-  );
-};
+const SignUp = () => <SignUpPage />;
 
 export default SignUp;
