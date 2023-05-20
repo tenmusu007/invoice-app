b856e1f9ba576c3283c21a615bfd7f744f04ab7b
Atsuya Tanaka <tenmusu007@gmail.com>
2023-05-19 17:59:06 -0700

    favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      {/* <Head /> */}
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
