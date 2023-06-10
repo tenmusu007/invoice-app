3f64dd95f23423a82fab9dd53f1a0f1335147a98
Yuto Yamakita <yuvto.0420@gmail.com>
2023-06-09 20:57:21 -0700

    Add meta description and title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Invoicer</title>
+        <meta
+          name="description"
+          content="Tired of creating invoice from the scratch? Now is the time to start using our Invoiceapp. This app allows you to create a invoice with a saved templates."
+        />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </Head>
       <body>
